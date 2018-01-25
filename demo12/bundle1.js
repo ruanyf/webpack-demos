eff09b0ee155d5c7f7acb4d8fd24f7d6ca32f611
huixisheng <huixisheng@huixishengdeMacBook-Pro.local>
2018-01-25 16:30:30 +0800

    feat: update demo12

---
+++ demo12/bundle1.js
@@ -0,0 +1,19 @@
+webpackJsonp([1],{
+
+/***/ 15:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var React = __webpack_require__(2);
+var ReactDOM = __webpack_require__(8);
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello World'
+), document.getElementById('a'));
+
+/***/ })
+
+},[15]);
---
+++ demo12/bundle2.js
@@ -0,0 +1,19 @@
+webpackJsonp([0],{
+
+/***/ 27:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var React = __webpack_require__(2);
+var ReactDOM = __webpack_require__(8);
+ReactDOM.render(React.createElement(
+  'h2',
+  null,
+  'Hello Webpack'
+), document.getElementById('b'));
+
+/***/ })
+
+},[27]);
